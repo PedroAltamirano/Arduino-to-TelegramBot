bfe4b08655c94ad37db8f68b21db5df01b83c814
PedroAltamirano <pedroaal04@gmail.com>
2021-01-09 20:06:33 -0500

    initial commit

---
+++ bot.ino
@@ -0,0 +1,70 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>   // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <ArduinoJson.h>
+
+// Humidity sensor
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+// Replace with your network credentials
+const char* ssid = "Oficina";
+const char* password = "2292922000";
+ 
+// Initialize Telegram BOT
+#define BOTtoken "1459070941:AAFd8_CO8RBrww6ioq_-yq5BEZ3zv7k9bGg"  // your Bot Token (Get from Botfather)
+ 
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID "872623800"
+ 
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+ 
+// Checks for new messages every 1 second.
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+ 
+const int ledPin = 5;
+bool ledState = LOW;
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  pinMode(DHTPIN, INPUT);
+  dht.begin(); // initialize the sensor
+ 
+  #ifdef ESP8266
+    client.setInsecure();
+  #endif
+ 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, ledState);
+   
+  // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+}
+ 
+void loop() {
+  if (millis() > lastTimeBotRan + botRequestDelay)  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+ 
+    while(numNewMessages) {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeBotRan = millis();
+  }
+}
---
+++ temp.ino
@@ -0,0 +1,25 @@
+void getTemp(void){
+  // read humidity
+  float humi  = dht.readHumidity();
+  // read temperature as Celsius
+  float tempC = dht.readTemperature();
+  // read temperature as Fahrenheit
+  float tempF = dht.readTemperature(true);
+  
+  // check if any reads failed
+  if (isnan(humi) || isnan(tempC) || isnan(tempF)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    Serial.print("Humidity: ");
+    Serial.print(humi);
+    Serial.print("%");
+    
+    Serial.print("  |  "); 
+    
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.print("°C ~ ");
+    Serial.print(tempF);
+    Serial.println("°F");
+  }
+}
